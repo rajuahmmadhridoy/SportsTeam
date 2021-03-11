a43a3479170acd413c83f4f991e444058b38383f
rajuahmmadhridoy <rajuahmmad0187@gmail.com>
2021-03-11 20:08:35 +0600

    netlify error solve2

--- src/Components/Teamlist/Teamlist.js
+++ src/Components/Teamlist/Teamlist.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { Button,Card } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faCoffee,faArrowRight } from '@fortawesome/free-solid-svg-icons'
+import {faArrowRight } from '@fortawesome/free-solid-svg-icons'
 import './Teamlist.css'
 const Teamlist = (props) => {
     // console.log(props.team)
